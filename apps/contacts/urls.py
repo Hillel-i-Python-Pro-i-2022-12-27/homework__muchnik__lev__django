3f78dc4373dca46f757540500b5b72305f1e5e11
levesin <lionrav19@gmail.com>
2023-03-03 00:12:11 +0200

    homework_10

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 app_name = "contacts"
 
-urlpatterns = [path("list/", views.list_contacts, name="list")]
+urlpatterns = [
+    path("list/", views.list_contacts, name="list"),
+    path("contacts-create/", views.ContactCreateView.as_view(), name="create"),
+    path("contacts-update/<int:pk>/", views.ContactUpdateView.as_view(), name="update"),
+    path("contactsdelete/<int:pk>/", views.ContactDeleteView.as_view(), name="delete"),
+    path("details/<int:pk>/", views.ContactDetailView.as_view(), name="details"),
+
+]
