affa7a91f7e40af88ffffeab8d1aa0d36c791643
levesin <lionrav19@gmail.com>
2023-03-03 13:34:09 +0200

    homework_10

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from apps.contacts.models import Contact
