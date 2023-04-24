afeaa8966e3639eb87ef2309b575fa086ffe4c73
levesin <lionrav19@gmail.com>
2023-04-24 12:21:09 +0300

    homework 16

--- apps/middleware_counter/urls.py
+++ apps/middleware_counter/urls.py
@@ -6,4 +6,6 @@ app_name = "logger-counter"
 
 urlpatterns = [
     path("", views.LogsListView.as_view(), name="list"),
+    path("list-users/", views.UserListView.as_view(), name="list_user"),
+    path("list-sessions/", views.SessionsListView.as_view(), name="list_sessions"),
 ]
